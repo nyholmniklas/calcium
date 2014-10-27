4ba5c36db8fbbb69c568a11177b13d6806156710
foldyy <Moshans@gmail.com>
2014-10-27 19:57:41 +0200

    added missing functions

--- calcium.js
+++ calcium.js
@@ -1,3 +1,14 @@
 function buttonPressed(number) {
     alert('Hello the number is ' + number);
 }
+
+
+function buttonSubstractPressed() {}
+
+function buttonAddPressed() {}
+
+function buttonDividePressed() {}
+
+function buttonMultiplyPressed() {}
+
+function buttonEqualsPressed() {}
