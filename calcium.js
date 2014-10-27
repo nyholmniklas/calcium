94d455ed42d6d95d54b2d59605b7875b2d196fdf
Niklas <nyholmniklas@gmail.com>
2014-10-27 20:29:21 +0200

    Clear screen hooray

--- calcium.js
+++ calcium.js
@@ -1,20 +1,37 @@
 var display = "";
+var pendingOperation = "";
+var leftVariable = "";
 
 function buttonPressed(number) {
     addToDisplay(number);
     var displayElement = document.getElementById('display');
-    displayElement.innerHTML = display;
+    displayElement.textContent = display;
 }
 
-function buttonSubstractPressed() {}
+function buttonSubstractPressed() {
+    pendingOperation = "substract";
+    storeAndClear();
+}
 
-function buttonAddPressed() {}
+function buttonAddPressed() {
+    pendingOperation = "add";
+}
 
-function buttonDividePressed() {}
+function buttonDividePressed() {
+    pendingOperation = "divide";
+}
 
-function buttonMultiplyPressed() {}
+function buttonMultiplyPressed() {
+    pendingOperation = "multiply";
+}
 
 function buttonEqualsPressed() {}
+
+function storeAndClear(){
+    var displayElement = document.getElementById('display');
+    leftVariable = displayElement.textContent;
+    displayElement.textContent = "";
+}
 
 function addToDisplay(number){
    display += number;
