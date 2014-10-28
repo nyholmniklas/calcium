ed71c49c6d1dd4d9bfc8d9d6108de5d1ffd6711d
Niklas <nyholmniklas@gmail.com>
2014-10-28 19:21:20 +0200

    Added support for numpad keys and operands

--- calcium.js
+++ calcium.js
@@ -46,13 +46,13 @@ function buttonEqualsPressed() {
             display = tempLeft * +getDisplayValue();
             break;
     }
-    
+
     pendingOperation = "";
     updateDisplay();
     store();
 }
 
-function buttonDecimalPressed(){
+function buttonDecimalPressed() {
     addDecimal();
     updateDisplay();
 }
@@ -84,19 +84,75 @@ function store() {
 }
 
 function addToDisplay(number) {
-    if (display == "0" || display == "Infinity" || display=="NaN") {
+    if (display == "0" || display == "Infinity" || display == "NaN") {
         display = "";
     }
     display = display.toString();
     display += number;
 }
 
-function addDecimal(){
+function addDecimal() {
     displayString = "" + display;
     if (displayString.indexOf(".") > -1) {
         return;
     }
     else {
         display += ".";
+    }
+}
+
+document.onkeydown = keyHandler;
+function keyHandler(e) {
+    var pressedKey;
+    if (document.all) {
+        e = window.event;
+        pressedKey = e.keyCode;
+    }
+    if (e.which) {
+        pressedKey = e.which;
+    }
+
+    //Keycode for enter
+    var enter = 13;
+    //Some keycodes for numpad keys on the keyboard
+    var numpad0KeyCode = 96;
+    var numpad9KeyCode = 105;
+    var numpadMultiply = 106;
+    var numpadAdd = 107;
+    var numpadSubstract = 109;
+    var numpadDecimal = 110;
+    var numpadDivide = 111;
+
+
+    //If enter was pressed call the appropriate function
+    if (pressedKey == enter) buttonEqualsPressed();
+    //Else if it was a number or operand button on the keypad do some stuff with magic ints
+    else if (numpad0KeyCode <= pressedKey <= numpadDivide) {
+        //If its a number
+        if (pressedKey < numpad9KeyCode) {
+            var adjustementToGetNumberFromKeyCode = 96;
+            var number = pressedKey - adjustementToGetNumberFromKeyCode;
+            buttonPressed(number);
+        }
+        //Else if its not a number and it's something like divide or enter..
+        else {
+            switch (pressedKey) {
+                case numpadMultiply:
+                    buttonMultiplyPressed();
+                    break;
+                case numpadDivide:
+                    buttonDividePressed();
+                    break;
+                case numpadAdd:
+                    buttonAddPressed();
+                    break;
+                case numpadSubstract:
+                    buttonSubstractPressed();
+                    break;
+                case numpadDecimal:
+                    buttonPressed(".");
+                    break;
+            }
+        }
     }
 }
